287f0e58bd7d2c528f25437a121dc7bc051bb5d0
Jordan Triplett <JordanT111596@gmail.com>
2020-08-11 16:08:38 -0400

    Variables log userinput and time slot where it was input

--- assets/script.js
+++ assets/script.js
@@ -7,60 +7,67 @@ $("#currentDay").text("Today is " + (moment().format("dddd, MMMM Do, YYYY")));
 
 //create a loop out of this
 for (var i = 0; i < timeArr.length; i++) {
-    
-//creates row for time block
-var newRow = $("<div>");
-//adds css class to row
-newRow.addClass("row time-block");
-//appends row to the container
-$(".container").append(newRow);
 
-//creates hour block
-var timeBlock = $("<div>");
-//adds css class to time block for hour
-timeBlock.addClass("hour col-md-1");
-//sets text to the time of the block
-timeBlock.text(timeArr[i]);
-//appends the hour to the time block row
-newRow.append(timeBlock);
+    //creates row for time block
+    var newRow = $("<div>");
+    //adds css class to row
+    newRow.addClass("row time-block");
+    //appends row to the container
+    $(".container").append(newRow);
 
-//creates text area box
-var newTextArea = $("<textarea>");
-//adds css class to text box
-newTextArea.addClass("description col-md-10");
-//assigns time data attribute
-newTextArea.attr("data-time", timeCheckArr[i]);
-//appends textarea to the time block row
-newRow.append(newTextArea);
+    //creates hour block
+    var timeBlock = $("<div>");
+    //adds css class to time block for hour
+    timeBlock.addClass("hour col-md-1");
+    //sets text to the time of the block
+    timeBlock.text(timeArr[i]);
+    //appends the hour to the time block row
+    newRow.append(timeBlock);
 
-//creates save button
-var saveButton = $("<button>");
-//creates save icon
-var saveIcon = $("<i>");
-//adds css class to text box
-saveButton.addClass("saveBtn col-md-1");
-//adds css class to save icon
-saveIcon.addClass("fas fa-save");
-//appends save button to the time block row
-newRow.append(saveButton);
-//appends save icon to the save button
-saveButton.append(saveIcon);
+    //creates text area box
+    var newTextArea = $("<textarea>");
+    //adds css class to text box
+    newTextArea.addClass("description col-md-10");
+    //assigns time data attribute
+    newTextArea.attr("data-time", timeCheckArr[i]);
+    //appends textarea to the time block row
+    newRow.append(newTextArea);
+
+    //creates save button
+    var saveButton = $("<button>");
+    //creates save icon
+    var saveIcon = $("<i>");
+    //adds css class to text box
+    saveButton.addClass("saveBtn col-md-1");
+    //adds css class to save icon
+    saveIcon.addClass("fas fa-save");
+    //appends save button to the time block row
+    newRow.append(saveButton);
+    //appends save icon to the save button
+    saveButton.append(saveIcon);
 
 
 
-//sets box color based on time
-//if the time is before the current time, make it a grey past box
-if (newTextArea.attr("data-time") < parseInt((moment().format("H")))) {
-    newTextArea.addClass("past");
+    //sets box color based on time
+    //if the time is before the current time, make it a grey past box
+    if (newTextArea.attr("data-time") < parseInt((moment().format("H")))) {
+        newTextArea.addClass("past");
+    }
+
+    //if the time is after the current time, make it a green future box
+    else if (newTextArea.attr("data-time") > parseInt((moment().format("H")))) {
+        newTextArea.addClass("future");
+    }
+
+    //if the time box is at the current time, make it a red present box
+    else {
+        newTextArea.addClass("present");
+    }
 }
 
-//if the time is after the current time, make it a green future box
-else if (newTextArea.attr("data-time") > parseInt((moment().format("H")))) {
-    newTextArea.addClass("future");
-}
-
-//if the time box is at the current time, make it a red present box
-else {
-    newTextArea.addClass("present");
-}
-}
+$(".saveBtn").on("click", function() {
+    var userText = $(this).siblings(".description").val();
+    var timeSlot = $(this).siblings(".description").attr("data-time");
+    console.log(userText);
+    console.log(timeSlot);
+});
